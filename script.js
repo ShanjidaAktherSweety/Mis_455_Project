e6561e0cf19103b18ec4ce26b658c609e6aea573
ShanjidaAktherSweety <shanjidasweet5@gmail.com>
2025-03-22 21:46:26 +0600

    add the some function in javascript page

---
+++ script.js
@@ -0,0 +1,13 @@
+function showAlert() {
+    alert("Hello! Welcome to My Shop.");
+}
+
+function addToCart() {
+    alert("Product added to cart!");
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("Thank you! Your message has been submitted.");
+}
+
